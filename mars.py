d7eeb2ac043dbd0bfbf59b2f63b9819839fa2469
Futsy <Futseh@users.noreply.github.com>
2017-11-20 12:55:26 +0100

    removed unecessary stuff

--- mars.py
+++ mars.py
@@ -3,14 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 1000
+n = 10000
 I = 9503
-bkg = 0
-rsun = 695700 * 10**3
-surfsun = 4 * np.pi * rsun**2
-dmars = 227940000 * 10**3
-drm = dmars / rsun
-Ism = I / (4 * np.pi * drm**2)
+bkg = 207
 sp = 25 * 10**(-3)
 sm = 600 * 10**(-3)
 
@@ -24,7 +19,7 @@ x = np.zeros(len(l))
 xp = np.linspace(0.15, 0.5, n)
 Ip = np.zeros(len(xp))
 
-mu = np.log(Imars / I) / (-l)
+mu = np.log(Imars / (I-bkg)) / (-l)
 d = np.log(2) / mu
 x = np.log(sp / sm) / (-mu)
 
@@ -36,14 +31,16 @@ Ip = I * np.exp(-mua * xp)
 
 stp = sm * np.exp(-mua * xp)
 
-print(mua, "\n", da, "\n", xa)
+print(mua, "My [m^-1]\n", da, "Halverditykkelse [m]\n", xa, "Distanse [m]")
 
 bol = True
 
 for i in range(len(xp)):
     if stp[i] <= sp and bol == True:
-        print("\n", stp[i], "\n", xp[i])
+        print("\n", stp[i], "Stråling [Sv]\n", xp[i], "Distanse under bakken [m]")
         bol = False
 
 plt.plot(xp, stp)
+plt.xlabel("Distanse under bakken [m]")
+plt.ylabel("Stråling [Sv]")
 plt.show()
